5325486156a53dfc30f574bffeab24ecac526838
Andrew Li <andrew.li1987@yandex.com>
2017-10-23 23:19:21 -1000

    Add files via upload

---
+++ getfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# A simple script
+
+mydir=$1;
+total=0;
+echo 'finding in '$mydir;
+for file in $1/*.zip
+do
+  info=$( zipinfo $file | grep -o "number of entries.*" | awk -F  ": " '/1/ {print $2}');
+  total=$(( total + info ));
+  echo $info;
+done
+echo 'total:' $total;
