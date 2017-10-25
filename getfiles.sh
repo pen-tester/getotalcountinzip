1e4a8fa0f755153591177da39d786f91bcf3b577
Andrew Li <andrew.li1987@yandex.com>
2017-10-24 18:10:27 -1000

    Update getfiles.sh

--- getfiles.sh
+++ getfiles.sh
@@ -4,11 +4,15 @@
 mydir=$1;
 total=0;
 echo 'finding in '$mydir;
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
 #for file in $1/?**/*.zip
-for file in $(find $mydir -name "*.zip")
+for file in $(find $mydir -iname \*.zip)
 do
-  info=$( zipinfo $file | grep -o "number of entries.*" | awk -F  ": " '/1/ {print $2}');
+#  echo "zipinfo  '$file'";
+  info=$( zipinfo -m "$file" | grep -o "number of entries: [0-9]*" ); # | awk -F  ": " '/1/ {print $2}');
+  info=$( echo "$info" | grep -o "[0-9]*");
   total=$(( total + info ));
-  echo $info;
+  echo "entry:$info  filename:$file";
 done
-echo 'total:' $total;
+echo "total: $total";
